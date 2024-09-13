a39fd717497058cc89861b2166e630898b93c11b
bikokwa <34458781+bikokwa@users.noreply.github.com>
2024-09-13 11:44:22 +0300

    using a data loader

--- src/datasources/listing-api.ts
+++ src/datasources/listing-api.ts
@@ -1,8 +1,24 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
+import DataLoader from "dataloader";
 import { Listing, Amenity, CreateListingInput } from "../types";
 
 export class ListingAPI extends RESTDataSource {
   baseURL = "https://rt-airlock-services-listing.herokuapp.com/";
+
+  private batchAmenities = new DataLoader(
+    async (listingIds): Promise<Amenity[][]> => {
+      console.log("Making one batched call with ", listingIds);
+
+      const amenitiesLists = await this.get<Amenity[][]>("amenities/listings", {
+        params: {
+          ids: listingIds.join(","),
+        },
+      });
+      console.log(amenitiesLists);
+
+      return amenitiesLists;
+    }
+  );
 
   getFeaturedListings(): Promise<Listing[]> {
     console.log("Calling for featured listings");
@@ -14,15 +30,17 @@ export class ListingAPI extends RESTDataSource {
   }
 
   getAmenities(listingId: string): Promise<Amenity[]> {
-    console.log("Making a follow-up call for amenities with ", listingId);
-    return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+    // console.log("Making a follow-up call for amenities with ", listingId);
+    console.log("Passing listing ID to the data loader ", listingId);
+    // return this.get<Amenity[]>(`listings/${listingId}/amenities`);
+    return this.batchAmenities.load(listingId);
   }
 
   createListing(listing: CreateListingInput): Promise<Listing> {
     return this.post("listings", {
       body: {
-        listing
-      }
+        listing,
+      },
     });
   }
 }
